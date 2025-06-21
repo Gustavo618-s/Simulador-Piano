c800e2a6ac5c86231ff4ef27e4b5e18323f13810
Gustavo Souza <ss.gustavo2007@gmail.com>
2025-06-21 16:50:40 -0300

    Iniciando JavaScript

---
+++ src/scripts/engine.js
@@ -0,0 +1,11 @@
+const pianoKeys = document.querySelectorAll(".piano-keys .key");
+let audio = new audio("src/tunes/a.wav");
+
+const playTune = (key) => {
+  audio.src = `src/tunes/${key}.wav`;
+  audio.play();
+};
+pianoKeys.forEach((key) => {
+  console.log(key.dataset.key);
+  key.addEventListener("click", () => playTune(key.dataset.key));
+});
